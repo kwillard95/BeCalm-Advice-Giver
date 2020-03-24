f33d3769b81c878eef93ce926024aedf1f1c3c01
kwillard95 <kwillard95@gmail.com>
2020-03-24 10:02:18 -0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div class="container">
+    <header id="header">
+      <h1>
+      BeCalm<i class="far fa-heart"></i>
+      </h1>
+    </header>
+    </div>
+  )
+}
